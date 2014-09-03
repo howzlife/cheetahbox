74134441f4a8f476f97f842cdaf7877bdabf61f3
Nick Dubus <nico_dubus@hotmail.com>
2014-09-02 22:26:03 -0400

    added scaffold for Asset from nifty

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140517025555) do
+ActiveRecord::Schema.define(version: 20140903022412) do
+
+  create_table "assets", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "assets", ["user_id"], name: "index_assets_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
